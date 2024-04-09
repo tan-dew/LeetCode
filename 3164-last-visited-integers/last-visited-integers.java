f2c56c0eb9d80ae3fd726944862042e7049d8504
Tanmoy Dewanjee <79822954+tan-dew@users.noreply.github.com>
2024-04-10 02:27:45 +0600

    Time: 1 ms (100.00%) | Memory: 44.3 MB (75.63%) - LeetSync

--- 3164-last-visited-integers/last-visited-integers.java
+++ 3164-last-visited-integers/last-visited-integers.java
@@ -7,7 +7,7 @@ class Solution {
         int k = 0;
         for(int num: nums){
             if (num > 0){
-                seen[seen.length - 1 - s] = num;
+                seen[s] = num;
                 s++;
                 k = 0;
             }
@@ -15,7 +15,7 @@ class Solution {
                 k++;
                 
                 if (k <= s){
-                    ans.add(seen[(seen.length - 1) - s + k]);
+                    ans.add(seen[s - k]);
                 }
                 else {
                     ans.add(num);
