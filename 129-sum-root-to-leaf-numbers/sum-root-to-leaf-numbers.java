5b1f20178e5b25717aa56b86c1022bb389e83aa4
Tanmoy Dewanjee <79822954+tan-dew@users.noreply.github.com>
2024-04-15 18:23:35 +0600

    Time: 0 ms (100.00%) | Memory: 41.3 MB (31.71%) - LeetSync

--- 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ 129-sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -14,34 +14,21 @@
  * }
  */
 class Solution {
-    public int sumNumbers(TreeNode root) {
-        if (root == null){
-            return 0;
-        }
-        
+    public int sumNumbers(TreeNode root) {        
         return sum(root, 0);
     }
     
     public int sum(TreeNode root, int sum){
         if (root == null){
+            return 0;
+        }
+        
+        sum = (sum * 10) + root.val;
+        
+        if (root.left == null && root.right == null){
             return sum;
         }
         
-        sum *= 10;
-        sum += root.val;
-        
-        if (root.left == null && root.right != null){
-            return sum(root.right, sum);
-        }
-        
-        if (root.right == null && root.left != null){
-            return sum(root.left, sum);
-        }
-        
-        if (root.left != null && root.right != null){
-            return sum(root.left, sum) + sum(root.right, sum);
-        }
-        
-        return sum;
+        return sum(root.left, sum) + sum(root.right, sum);
     }
 }
