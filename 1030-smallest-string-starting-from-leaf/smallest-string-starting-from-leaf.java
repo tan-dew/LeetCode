beecf7cc2d10559aff7aae2cd595b5822ce88166
Tanmoy Dewanjee <79822954+tan-dew@users.noreply.github.com>
2024-04-17 17:17:30 +0600

    Time: 4 ms (49.48%) | Memory: 44.5 MB (55.56%) - LeetSync

---
+++ 1030-smallest-string-starting-from-leaf/smallest-string-starting-from-leaf.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    
+    List<String> strs = new ArrayList<>();
+    
+    public String smallestFromLeaf(TreeNode root) {
+        if (root == null){
+            return "";
+        }
+        
+        dfs(root, new StringBuilder());
+        
+        Collections.sort(strs);
+        
+        return strs.get(0);
+    }
+    
+    public void dfs(TreeNode root, StringBuilder sb){
+        
+        sb.append((char) ('a' + root.val));
+        
+        if (root.left == null && root.right == null){
+            strs.add(sb.reverse().toString());
+        }
+        
+        if (root.left != null){
+            dfs(root.left, new StringBuilder(sb));
+        }
+        
+        if (root.right != null){
+            dfs(root.right, new StringBuilder(sb));
+        }
+    }
+}
