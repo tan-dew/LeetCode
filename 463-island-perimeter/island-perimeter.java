09a636ca4844b20d811266b130f1da37eeda01f1
Tanmoy Dewanjee <79822954+tan-dew@users.noreply.github.com>
2024-04-18 16:30:58 +0600

    Time: 8 ms (18.66%) | Memory: 45.7 MB (55.18%) - LeetSync

--- 463-island-perimeter/island-perimeter.java
+++ 463-island-perimeter/island-perimeter.java
@@ -1,44 +1,32 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
-        int perimeter = 0;
         
         for (int row = 0; row < grid.length; row++){
             for (int col = 0; col < grid[row].length; col++){
                 if (grid[row][col] == 1){
-                    if (row == 0){
-                        perimeter = perimeter + 1;
-                    }
-                    
-                    if (row == grid.length - 1){
-                        perimeter = perimeter + 1;
-                    }
-                    
-                    if (col == 0){
-                        perimeter = perimeter + 1;
-                    }
-                    
-                    if (col == grid[row].length - 1){
-                        perimeter = perimeter + 1;
-                    }
-                    
-                    if (row > 0 && grid[row - 1][col] == 0){
-                        perimeter = perimeter + 1;
-                    }
-                    
-                    if (row < grid.length - 1 && grid[row + 1][col] == 0){
-                        perimeter = perimeter + 1;
-                    }
-                    
-                    if (col > 0 && grid[row][col - 1] == 0){
-                        perimeter = perimeter + 1;
-                    }
-                    
-                    if (col < grid[row].length - 1 && grid[row][col + 1] == 0){
-                        perimeter = perimeter + 1;
-                    }
+                    return dfs(grid, row, col, new boolean[grid.length][grid[row].length]);
                 }
             }
         }
+        
+        return 0;
+    }
+    
+    int dfs(int[][] grid, int row, int col, boolean[][] visited){
+        if (row < 0 || row > grid.length - 1 || col < 0 || col > grid[row].length - 1 || grid[row][col] == 0){
+            return 1;
+        }
+        
+        if (visited[row][col]){
+            return 0;
+        }
+        
+        visited[row][col] = true;
+        
+        int perimeter = dfs(grid, row - 1, col, visited) 
+            + dfs(grid, row, col + 1, visited) 
+            + dfs(grid, row + 1, col, visited)
+            + dfs(grid, row, col - 1, visited);
         
         return perimeter;
     }
