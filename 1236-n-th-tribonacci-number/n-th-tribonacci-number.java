7874853780bc0a93f4c82b596d947eb3645e52c9
Tanmoy Dewanjee <79822954+tan-dew@users.noreply.github.com>
2024-04-24 22:10:33 +0600

    Time: 0 ms (100.00%) | Memory: 40.8 MB (8.00%) - LeetSync

--- 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
+++ 1236-n-th-tribonacci-number/n-th-tribonacci-number.java
@@ -1,38 +1,15 @@
 class Solution {
     public int tribonacci(int n) {
-        int[] trib = new int[n + 1];
+        int length = Math.max(n + 1, 3);
+        int[] tribonacci = new int[length];
+        tribonacci[0] = 0;
+        tribonacci[1] = 1;
+        tribonacci[2] = 1;
         
-        Arrays.fill(trib, -1);
-        
-        return trib(n, trib);
-    }
-    
-    int trib(int n, int[] trib){
-        if (n < 0){
-            return 0;
+        for (int i = 3; i <= n; i++){
+            tribonacci[i] = tribonacci[i - 1] + tribonacci[i - 2] + tribonacci[i - 3];
         }
         
-        if (n == 0){
-            trib[n] = 0;
-            return trib[n];
-        }
-        
-        if (n == 1){
-            trib[n] = 1;
-            return trib[n];
-        }
-        
-        if (n == 2){
-            trib[n] = 1;
-            return trib[n];
-        }
-        
-        if(trib[n] != -1){
-            return trib[n];
-        }
-        
-        trib[n] = trib(n - 1, trib) + trib(n - 2, trib) + trib(n - 3, trib);
-        
-        return trib[n];
+        return tribonacci[n];
     }
 }
